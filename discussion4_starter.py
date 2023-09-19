e00728ea8eca0b2e23bbb0041315a550c9466dcf
phoebeanoh <129363790+phoebeanoh@users.noreply.github.com>
2023-09-19 14:04:11 -0400

    Add files via upload
    Even though I put it in the repo, it would not upload

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -6,6 +6,9 @@ class Rectangle():
     # It sets an instance variable, "height" to the passed argument, height
 
     # YOUR CODE HERE
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
 
@@ -14,7 +17,8 @@ class Rectangle():
     # It returns a string, 
     #       "A rectangle with width ____ and height ____"
 
-    # YOUR CODE HERE
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
 
 
@@ -25,7 +29,13 @@ class Rectangle():
     #       False otherwise
 
     # YOUR CODE HERE
-
+    def verify_input(self):
+        # Check if width and height are positive numbers
+        if self.width > 0 and self.height > 0:
+            return True
+        else:
+            return False
+        
 
 
     # Create the "area" method
@@ -34,6 +44,13 @@ class Rectangle():
     # Otherwise, it returns the area of the rectangle.
 
     # YOUR CODE HERE
+    def area(self):
+        # Calculate the area of the rectangle
+        if self.verify_input():
+            return self.width * self.height
+        else:
+            return "Invalid input"
+
 
 
 
@@ -43,7 +60,13 @@ class Rectangle():
     # Otherwise, it returns the perimeter of the rectangle.
 
     # YOUR CODE HERE
-    
+    def perimeter(self):
+        if self.verify_input():
+            return self.width * 2 + self.height * 2
+        else:
+            return "Invalid input"
+
+
 
 
 def main():
